5088df5efcb9a706b15f8f1f6db87933653d7851
Camylly Barros Silva <146674100+Camyllyb@users.noreply.github.com>
2026-01-15 15:45:03 +0000

    chore: instalar dependência nanoid e atualizações de projeto

---
+++ anybank/src/app/extrato/transacao/transacao.component.ts
@@ -0,0 +1,13 @@
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { Transacao } from '../../modelos/transacao';
+
+@Component({
+  selector: 'app-transacao',
+  imports: [CurrencyPipe, DatePipe],
+  templateUrl: './transacao.component.html',
+  styleUrl: './transacao.component.css'
+})
+export class TransacaoComponent {
+  transacao = input.required<Transacao>();
+}
